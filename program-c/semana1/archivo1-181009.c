3c37bbcc6a9f898ae4dfeb6c1e7cae44fc748779
DavidENRIQUE <david@localhost.localdomain>
2018-10-09 14:03:12 -0700

    Primer Programa

---
+++ program-c/semana1/archivo1-181009.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+int Numero;
+Numero = 1;
+
+
+printf("hola david\n");
+printf("hola david\t");
+printf("\nMi numero es %d",Numero);
+Numero = 3;
+printf("\nMi numero es %d",Numero);
+
+}
